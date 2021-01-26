fe8f2594e0ee9f1d6818023782e50f837f642f0e
colonca <c_colon10@hotmail.com>
2021-01-26 11:56:46 -0500

    metadescriptions

--- src/components/search.js
+++ src/components/search.js
@@ -25,6 +25,10 @@ function Search() {
         setKeyword(evt.target.value);
     }
 
+    const reset = (evt) => {
+        setKeyword('');
+    }
+
     return  (
             <div className="position-relative">
                 <div className="position-absolute">
@@ -32,7 +36,7 @@ function Search() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
                 </div>
-                <input type="text" onChange={handleChange} className="input_search" placeholder="search"/>
+                <input type="text" onChange={handleChange} className="input_search" placeholder="search" value={keyword}/>
                 {loading && <Spinner className="spinner mr-2 mt-2" animation="border" variant="warning" size="sm"/>}
                 {
                     keyword.length > 2 && (
@@ -43,7 +47,7 @@ function Search() {
                                     {
                                         movies.map(movie => (
                                             <li className="border-bottom border-secondary" key={movie.id}>
-                                                <Link to={`/${movie.id}`} className="d-flex align-items-center item text-white px-3 py-2 font-weight-bold">
+                                                <Link onClick={reset} to={`/${movie.id}`} className="d-flex align-items-center item text-white px-3 py-2 font-weight-bold">
                                                 {
                                                     movie.poster_path ? (
                                                         <img src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`} 
